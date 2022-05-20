8916e8fe3b4e78ac31ecd9abc299f83df3f6c0b5
ninghsiao <ninghsiao@tutanota.com>
2022-05-20 19:40:25 +0200

    Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,20 +5,20 @@ const Footer = () => {
     return (
         <div className="flex w-full justify-center items-center">
             <div className="flex md:flex-row flex-col items-start justify-between md:p-20 py-12 px-12">
-                <div lassName="flex flex-1 justify-start flex-col md:mr-10 sm:">
+                <div className="flex justify-start flex-col md:mr-10 sm:">
                     <h1 className="text-center text-3xl text-white py-1 font-bold -indent-5">
-                        Connect with us 
+                        Connect with us
                     </h1>
+                  <div className="flex justify-center">
                     <a href="https://t.me/homofinance">
-                            <img src={Telegram} alt="Telegram" className="inline pr-4 w-15">
-                            </img>
+                            <img src={Telegram} alt="Telegram" className="inline pr-4 w-15"/>
                     </a>
                     <a href="https://discord.gg/te6YMRW3mV">
                         <div className="inline relative">
-                            <img src={Discord} alt="Discord" className="inline pr-4 w-16">
-                            </img>
+                            <img src={Discord} alt="Discord" className="inline pr-4 w-16"/>
                         </div>
                     </a>
+                  </div>
                 </div>
             </div>
         </div>
